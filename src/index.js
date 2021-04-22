56a6f405285438b675f5fea8b9f343a8c5600755
higialuz <72925646+higialuz@users.noreply.github.com>
2021-04-22 18:56:39 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+import App from './App.js';
+
+const store = configureStore();
+const persistor = persistStore(store);
+	
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate
+            loading={<div>Loading...</div>}
+            persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
+    document.getElementById('root'),
+);
